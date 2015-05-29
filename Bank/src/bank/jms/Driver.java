89a7140923c7acc8df3703b12040d303bbea0131
Grisu118 <grisu118@grisu118.ch>
2015-05-29 21:52:26 +0200

    Nearly Finished JMS Bank

--- Bank/src/bank/jms/Driver.java
+++ Bank/src/bank/jms/Driver.java
@@ -1,5 +1,6 @@
 package bank.jms;
 
+
 import bank.*;
 
 import javax.jms.*;
@@ -7,6 +8,9 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.DoubleSummaryStatistics;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -15,37 +19,28 @@ import java.util.Set;
 public class Driver implements BankDriver2 {
 
     private static bank.Bank bank;
+    private JMSUpdateHandler handler;
 
     @Override
     public void registerUpdateHandler(UpdateHandler handler) throws IOException {
-
+        System.out.println("Register");
+        this.handler.registerUpdateHandler(handler);
     }
 
     @Override
     public void connect(String[] args) throws IOException {
+        bank = new Bank();
         try {
-            final Context jndiContext = new InitialContext();
-            final ConnectionFactory factory =
-                    (ConnectionFactory) jndiContext.lookup("ConnectionFactory");
-            final Queue queue = (Queue) jndiContext.lookup("/queue/BANK");
-            final Topic topic = (Topic) jndiContext.lookup("/topic/BANK");
-
-            try (JMSContext context = factory.createContext()) {
-                TemporaryQueue tempQueue = context.createTemporaryQueue();
-
-                JMSProducer sender = context.createProducer().setJMSReplyTo(tempQueue);
-                JMSConsumer receiver = context.createConsumer(tempQueue);
-                bank = new Bank(sender, receiver);
-            }
-
+            handler = new JMSUpdateHandler();
+            handler.start();
         } catch (NamingException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
     public void disconnect() throws IOException {
+        bank = null;
     }
 
     @Override
@@ -53,80 +48,167 @@ public class Driver implements BankDriver2 {
         return bank;
     }
 
-    static class Bank implements bank.Bank {
-        private JMSProducer sender;
-        private JMSConsumer receiver;
+    class Bank implements bank.Bank {
+        private ConnectionFactory connectionFactory;
+        private Context jndiContext;
         private Queue queue;
 
-        public Bank(JMSProducer sender, JMSConsumer receiver, Queue queue) {
-            this.sender = sender;
-            this.receiver = receiver;
-            this.queue = queue;
+        public Bank() {
+            try {
+                jndiContext = new InitialContext();
+                connectionFactory = (ConnectionFactory) jndiContext.lookup("ConnectionFactory");
+                final ConnectionFactory factory =
+                        connectionFactory;
+                queue = (Queue) jndiContext.lookup("/queue/BANK");
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        private <T> T send(String msg, Class<T> tClass) {
+            try (JMSContext context = connectionFactory.createContext()) {
+                TemporaryQueue tempQueue = context.createTemporaryQueue();
+
+                JMSProducer sender = context.createProducer().setJMSReplyTo(tempQueue);
+                JMSConsumer receiver = context.createConsumer(tempQueue);
+
+                sender.send(queue, msg);
+                return receiver.receiveBody(tClass);
+            }
         }
 
         @Override
         public String createAccount(String owner) throws IOException {
-            sender.send(queue, "CREATE");
-
-            return null;
+            if (owner == null || "".equals(owner)) { return null;}
+            return send("CREATEACC:" + owner, String.class);
         }
 
         @Override
         public boolean closeAccount(String number) throws IOException {
-            return false;
+            if (number == null || "".equals(number)) { return false; }
+            return send("CLOSEACC:" + number, Boolean.class);
         }
 
         @Override
         public Set<String> getAccountNumbers() throws IOException {
-            return null;
+            String s = send("GETACCNUMBERS:", String.class);
+            String[] a = s.split(":");
+            Set<String> set;
+            if (a.length > 0 && a.length <= 1 && "".equals(a[0])) {
+                set = new HashSet<>();
+            } else {
+                set = new HashSet<>(Arrays.asList(a));
+            }
+            return set;
         }
 
         @Override
         public bank.Account getAccount(String number) throws IOException {
-            return null;
+            if (number == null || number.equals("")) { return null;}
+            String msg = send("GETACC:" + number, String.class);
+            if ("NULL".equals(msg)) {
+                return null;
+            }
+            return new Account(msg.split(":"));
         }
 
         @Override
         public void transfer(bank.Account a, bank.Account b, double amount) throws IOException, IllegalArgumentException, OverdrawException, InactiveException {
+            String msg = send("TRANSFER:" + a.getNumber() + ":" + b.getNumber() + ":" + amount, String.class);
+            switch (msg) {
+                case "ILLEGAL":
+                    throw new IllegalArgumentException();
+                case "OVERDRAW":
+                    throw new OverdrawException();
+                case "INACTIVE":
+                    throw new InactiveException();
+                default:
+                    break;
+            }
+        }
 
+        class Account implements bank.Account {
+
+            private String number;
+            private String owner;
+            private double balance;
+            private boolean isActive;
+
+            public Account(String number, String owner, double balance, boolean isActive) {
+                this.number = number;
+                this.owner = owner;
+                this.balance = balance;
+                this.isActive = isActive;
+            }
+
+            public Account(String[] args) {
+                this.number = args[0];
+                this.owner = args[1];
+                this.balance = Double.parseDouble(args[2]);
+                System.out.println(balance);
+                this.isActive = Boolean.parseBoolean(args[3]);
+            }
+
+
+            @Override
+            public String getNumber() throws IOException {
+                return number;
+            }
+
+            @Override
+            public String getOwner() throws IOException {
+                return owner;
+            }
+
+            @Override
+            public boolean isActive() throws IOException {
+                return isActive;
+            }
+
+            @Override
+            public boolean setInactive() throws IOException {
+                if (balance > 0) {
+                    return false;
+                }
+                isActive = false;
+                return true;
+            }
+
+            @Override
+            public void deposit(double amount) throws IOException, IllegalArgumentException, InactiveException {
+                String msg = send("DEPOSIT:" + getNumber() + ":" + amount, String.class);
+                switch (msg) {
+                    case "ILLEGAL":
+                        throw new IllegalArgumentException();
+                    case "INACTIVE":
+                        throw new InactiveException();
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void withdraw(double amount) throws IOException, IllegalArgumentException, OverdrawException, InactiveException {
+                String msg = send("WITHDRAW:" + getNumber() + ":" + amount, String.class);;
+                switch (msg) {
+                    case "ILLEGAL":
+                        throw new IllegalArgumentException();
+                    case "OVERDRAW":
+                        throw new OverdrawException();
+                    case "INACTIVE":
+                        throw new InactiveException();
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public double getBalance() throws IOException {
+                return balance;
+            }
         }
     }
 
-    static class Account implements bank.Account {
 
-        @Override
-        public String getNumber() throws IOException {
-            return null;
-        }
-
-        @Override
-        public String getOwner() throws IOException {
-            return null;
-        }
-
-        @Override
-        public boolean isActive() throws IOException {
-            return false;
-        }
-
-        @Override
-        public boolean setInactive() throws IOException {
-            return false;
-        }
-
-        @Override
-        public void deposit(double amount) throws IOException, IllegalArgumentException, InactiveException {
-
-        }
-
-        @Override
-        public void withdraw(double amount) throws IOException, IllegalArgumentException, OverdrawException, InactiveException {
-
-        }
-
-        @Override
-        public double getBalance() throws IOException {
-            return 0;
-        }
-    }
 }
--- Bank/src/bank/jms/Server.java
+++ Bank/src/bank/jms/Server.java
@@ -1,39 +1,135 @@
 package bank.jms;
 
+import bank.Account;
+import bank.Bank;
+import bank.InactiveException;
+import bank.OverdrawException;
+import bank.Server.ServerBank;
+
 import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.io.IOException;
+import java.util.Set;
 
 /**
  * Created by benjamin on 29.05.2015.
  */
 public class Server {
 
+    private static Bank bank = new ServerBank();
+    private static ConnectionFactory factory;
+    private static Topic topic;
+
     public static void main(String[] args) {
 
         try {
             final Context jndiContext = new InitialContext();
-            final ConnectionFactory factory =
-                    (ConnectionFactory) jndiContext.lookup("ConnectionFactory");
+            factory = (ConnectionFactory) jndiContext.lookup("ConnectionFactory");
             final Queue queue = (Queue) jndiContext.lookup("/queue/BANK");
-            final Topic topic = (Topic) jndiContext.lookup("/topic/BANK");
+            topic = (Topic) jndiContext.lookup("/topic/BANK");
 
             try (JMSContext context = factory.createContext()) {
                 JMSConsumer consumer = context.createConsumer(queue);
-                //JMSProducer sender = context.createProducer(topic);
+                JMSProducer sender = context.createProducer();
 
                 System.out.println("Bank service is running...");
                 while(true) {
                     Message request = consumer.receive();
-                    System.out.println("Handle: " + request.getBody(String.class));
-                    //sender.send(request.getJMSReplyTo(), "Echo: " + request.getBody(String.class));
+                    String msg = request.getBody(String.class);
+                    System.out.println("Handle: " + msg);
+                    String[] a = msg.split(":");
+                    switch (a[0]) {
+                        case "GETACCNUMBERS" :
+                            Set<String> set = bank.getAccountNumbers();
+                            StringBuilder builder = new StringBuilder();
+                            for (String s : set) {
+                                builder.append(s);
+                                builder.append(':');
+                            }
+                            sender.send(request.getJMSReplyTo(), builder.toString());
+                            break;
+                        case "GETACC" :
+                            Account acc = bank.getAccount(a[1]);
+                            if (acc == null) {
+                                sender.send(request.getJMSReplyTo(), "NULL");
+                            } else {
+                                sender.send(request.getJMSReplyTo(), acc.getNumber() + ":" + acc.getOwner() + ":" + acc.getBalance() + ":" + acc.isActive());
+                            }
+                            break;
+                        case "CREATEACC" :
+                            String number = bank.createAccount(a[1]);
+                            System.out.println("Account Created: " + number);
+                            sender.send(request.getJMSReplyTo(), number);
+                            update(number);
+                            break;
+                        case "CLOSEACC" :
+                            boolean result = bank.closeAccount(a[1]);
+                            sender.send(request.getJMSReplyTo(), result);
+                            update(a[1]);
+                            break;
+                        case "TRANSFER" :
+                            Account from = bank.getAccount(a[1]);
+                            Account to = bank.getAccount(a[2]);
+                            Double amount = Double.parseDouble(a[3]);
+                            try {
+                                bank.transfer(from, to, amount);
+                                sender.send(request.getJMSReplyTo(), "DONE");
+                                update(from.getNumber());
+                                update(to.getNumber());
+                            } catch (IllegalArgumentException e) {
+                                sender.send(request.getJMSReplyTo(), "ILLEGAL");
+                            } catch (OverdrawException e) {
+                                sender.send(request.getJMSReplyTo(), "OVERDRAW");
+                            } catch (InactiveException e) {
+                                sender.send(request.getJMSReplyTo(), "INACTIVE");
+                            }
+                            break;
+                        case "DEPOSIT" : {
+                            Account account = bank.getAccount(a[1]);
+                            try {
+                                account.deposit(Double.parseDouble(a[2]));
+                                sender.send(request.getJMSReplyTo(), "DONE");
+                                update(account.getNumber());
+                            } catch (IllegalArgumentException e) {
+                                sender.send(request.getJMSReplyTo(), "ILLEGAL");
+                            } catch (InactiveException e) {
+                                sender.send(request.getJMSReplyTo(), "INACTIVE");
+                            }
+                        }
+                            break;
+                        case "WITHDRAW" :
+                            Account account = bank.getAccount(a[1]);
+                            try {
+                                account.withdraw(Double.parseDouble(a[2]));
+                                sender.send(request.getJMSReplyTo(), "DONE");
+                                update(account.getNumber());
+                            } catch (IllegalArgumentException e) {
+                                sender.send(request.getJMSReplyTo(), "ILLEGAL");
+                            } catch (OverdrawException e) {
+                                sender.send(request.getJMSReplyTo(), "OVERDRAW");
+                            } catch (InactiveException e) {
+                                sender.send(request.getJMSReplyTo(), "INACTIVE");
+                            }
+                            break;
+                    }
+
+
                 }
             } catch (JMSException e) {
                 e.printStackTrace();
             }
-        } catch (NamingException e) {
+        } catch (NamingException | IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void update(String number) {
+        try (JMSContext context = factory.createContext()) {
+            JMSProducer publisher = context.createProducer();
+            System.out.println("\tSended Update: " + number);
+            publisher.send(topic, number);
         }
     }
 
